31f8af2ca40d42641391bae05544598e4585bdf4
gopal-nd <gopalhelpyou@gmail.com>
2024-09-15 14:58:47 +0530

    video details using the yt api key

--- components/YouTubePlayer.tsx
+++ components/YouTubePlayer.tsx
@@ -1,90 +1,127 @@
+import { FC, useState } from "react";
+import YouTube, { YouTubeProps, YouTubeEvent } from "react-youtube";
+import { Button } from "./ui/button";
 
-import {FC, useState} from 'react'
-import YouTube, { YouTubeProps ,YouTubeEvent} from 'react-youtube'
-import { Button } from './ui/button'
-
-
-interface YoutubePlayerProps {
-    playlist: string[]
+interface Video {
+  id: string;
+  title: string;
+  thumbnail: string;
 }
 
-const YouTubePlayer: FC<YoutubePlayerProps> = ({playlist}) => {
+interface YouTubePlayerProps {
+  playlist: Video[];
+  setPlaylist: (playlist: Video[]) => void;
+}
 
-    const [currentVideoIndex, setCurrentVideoIndex] = useState<number>(0)
+const YouTubePlayer: FC<YouTubePlayerProps> = ({ playlist, setPlaylist }) => {
+  const [currentVideoIndex, setCurrentVideoIndex] = useState<number>(0);
 
-    const handleVideoEnd = (event: YouTubeEvent) => {
-        if (currentVideoIndex < playlist.length - 1) {
-          setCurrentVideoIndex(currentVideoIndex + 1); // Move to the next video
-        }
-      };
-    
+  const handleVideoEnd = (event: YouTubeEvent) => {
+    if (currentVideoIndex < playlist.length - 1) {
+      setCurrentVideoIndex(currentVideoIndex + 1); // Move to the next video
+    }
+  };
+
+  const handleRemove = (index: number) => {
+    const updatedPlaylist = playlist.filter((_, i) => i !== index);
+    setPlaylist(updatedPlaylist);
+
+    if (currentVideoIndex === index) {
+      // Adjust currentVideoIndex if the currently playing video is removed
+      setCurrentVideoIndex(Math.max(0, currentVideoIndex - 1));
+    }
+  };
+
   const opts = {
-    height: '390',
-    width: '640',
+    height: "390",
+    width: "640",
     playerVars: {
       autoplay: 1, // Autoplay the video
     },
   };
 
   return (
-    <div className="w-full flex flex-col justify-center my-6">
+    <div className="w-full flex flex-col lg:flex-row  gap-4 justify-center my-6">
+      {/* Display the currently playing video */}
+      <div className="w-full lg:w-1/2 mt-10 lg:mt-0 ">
+        <h2 className="text-xl font-semibold">Currently Playing</h2>
+        {playlist.length > 0 ? (
+          <div className="w-full mt-10 flex justify-center my-6">
+            <YouTube
+              videoId={playlist[currentVideoIndex].id}
+              opts={opts}
+              onEnd={handleVideoEnd}
+              className="shadow-lg rounded-lg"
+            />
+          </div>
+        ) : (
+          <p className="text-center text-gray-500">
+            No videos in the playlist.
+          </p>
+        )}
 
+        {/* Controls for navigating between videos */}
+        {playlist.length > 0 && (
+          <div className="flex mt-6 justify-center items-center gap-3 text-center">
+            {currentVideoIndex > 0 ? (
+              <Button
+                onClick={() => setCurrentVideoIndex(currentVideoIndex - 1)}
+              >
+                prev
+              </Button>
+            ) : (
+              <Button disabled>prev</Button>
+            )}
 
-         {/* to list the songs */}
-        {
-            playlist.length > 0 && (
-                <ul className=" ml-8 mt-4 list-none">
-                {playlist.map((id, index) => (
-                  <li key={index} className="text-gray-700 " onClick={() => setCurrentVideoIndex(index)}>
-                    <div className='text-lg font-bold cursor-pointer text-blue-600' >
+            <p>{currentVideoIndex + 1}</p>
 
-                  {index + 1}. video id = {id}
-                    </div>
-                
-                  </li>
-                ))}
-              </ul>
-            )
-        }
+            {currentVideoIndex < playlist.length - 1 ? (
+              <Button
+                onClick={() => setCurrentVideoIndex(currentVideoIndex + 1)}
+              >
+                next
+              </Button>
+            ) : (
+              <Button disabled>next</Button>
+            )}
+          </div>
+        )}
+      </div>
 
-        {/* to display and currently play the song */}
-
-    {playlist.length > 0 ? (
-        <div className="w-full mt-10 flex justify-center my-6">
-
-      <YouTube
-        videoId={playlist[currentVideoIndex]}
-        opts={opts}
-        onEnd={handleVideoEnd}
-        className="shadow-lg rounded-lg"
-        />
-        </div>
-    ) : (
-      <p className="text-center text-gray-500">No videos in the playlist.</p>
-    )}
-
-
-    {/* choosing the songs to play */}
-
- {playlist.length > 0 &&  <div className='flex mt-6 justify-center items-center gap-3 text-center'>
-
-        { currentVideoIndex > 0 ?
-        <Button onClick={() => setCurrentVideoIndex(currentVideoIndex - 1)}>prev</Button>:
-        <Button disabled>prev</Button>
-        }
-
-        <p>{currentVideoIndex + 1}</p>
-
-        {
-         currentVideoIndex < playlist.length - 1 ?
-        <Button onClick={() => setCurrentVideoIndex(currentVideoIndex + 1)}>next</Button>:
-        <Button disabled>next</Button>
-        }
+      {/* List the videos with title and thumbnail */}
+      <div className=" w-full lg:w-1/2">
+        <h2 className="text-xl font-semibold">Playlist</h2>
+        {playlist.length > 0 && (
+          <ul className="flex flex-col gap-4 mt-4 list-none ">
+            {playlist.map((video, index) => (
+              <li
+                key={index}
+                className="flex  items-start gap-4 cursor-pointer"
+              >
+                <img
+                  src={video.thumbnail}
+                  alt={video.title}
+                  className="w-30 h-30 shadow-lg rounded-lg"
+                  onClick={() => setCurrentVideoIndex(index)}
+                />
+                <div className="text-lg font-bold flex items-start justify-between flex-col gap-4 ">
+                  <p>
+                    {index + 1}. {video.title}
+                  </p>
+                  <Button
+                    onClick={() => handleRemove(index)}
+                    className="ml-4 bg-red-500 text-white hover:bg-red-600"
+                  >
+                    Remove
+                  </Button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
- }
-  </div>
   );
-}
+};
 
-
-export default YouTubePlayer
+export default YouTubePlayer;
